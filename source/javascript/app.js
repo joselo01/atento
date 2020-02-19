80bcb637e5c3452d8bd2f93d904aeb31ce38aff2
joselo01 <sjmujica@gmail.com>
2020-02-19 06:44:12 -0500

    update atento

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
     $(this).tab("show");
   });
   $(".input-group.date").datepicker({ format: "dd.mm.yyyy" });
+
+  $(".progressbar li").click(function() {
+    $("li").removeClass("active");
+    $(this).addClass("active");
+  });
+  $(".showSec").click(function() {
+    $(".content-estudios").show();
+  });
+  $(".hideSec").click(function() {
+    $(".content-estudios").hide();
+  });
 });
