bea807dce076c684d39201bb623924c94ff04d40
joselo01 <sjmujica@gmail.com>
2020-02-21 05:22:32 -0500

    update atento

--- source/javascript/app.js
+++ source/javascript/app.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-  $(".hamburger").click(function() {
+  $(".hamburger").on("click", function() {
     $(".hamburger").toggleClass("open");
   });
   $("#myModal").on("shown.bs.modal", function() {
     $("#myInput").focus();
   });
-  $("#myTabs a").click(function(e) {
+  $("#myTabs a").on("click", function(e) {
     e.preventDefault();
     $(this).tab("show");
   });
   $(".input-group.date").datepicker({ format: "dd.mm.yyyy" });
 
-  $(".progressbar li").click(function() {
+  $(".progressbar li").on("click", function() {
     $("li").removeClass("active");
     $(this).addClass("active");
   });
-  $(".showSec").click(function() {
+  $(".showSec").on("click", function() {
     $(".content-estudios").show();
   });
-  $(".hideSec").click(function() {
+  $(".hideSec").on("click", function() {
     $(".content-estudios").hide();
   });
 });
